02b194f777cc4ba1be034779f1669427fc7dd8a2
HashimAziz <hashimkayani97@gmail.com>
2021-03-22 09:02:10 +0500

    project complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import React, { useState } from "react";
+import items from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+//Set data structure in javascript accept the unique item
+const allcategories = ["all", ...new Set(items.map((item) => item.category))]; // get categories from item list
+
 function App() {
-  return <h2>Menu App</h2>;
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState(allcategories);
+
+  //filter item from item list base on category choose
+  const filterCategories = (category) => {
+    if (category === "all") {
+      setMenuItem(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItem(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          filterCategories={filterCategories}
+          categories={categories}
+        />
+        <Menu items={menuItem} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
